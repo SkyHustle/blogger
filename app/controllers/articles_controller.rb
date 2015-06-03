4574742904475dd839486f02d51c0c397ffa878f
Dmitry Sky <dmitry.sky@icloud.com>
2015-06-03 15:24:23 -0600

    creating and deleting articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,31 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.create(article_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
